7fdabf91b7f2a760c98d3e075a1e570864d2b8e8
Brian Uceda <brian.uceda@hotmail.com>
2026-01-05 00:05:20 -0500

    remove world

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 title Arclight 1.21.1 NeoForge Server
 echo Iniciando servidor...
 
-java -Xms4G -Xmx4G -jar arclight-neoforge-1.21.1-1.0.2-SNAPSHOT-2e9399c.jar nogui
+java -Xms8G -Xmx8G -jar arclight-neoforge-1.21.1-1.0.2-SNAPSHOT-2e9399c.jar nogui
 
 pause
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xms10G -Xmx10G -jar arclight-neoforge-1.21.1-1.0.2-SNAPSHOT-2e9399c.jar nogui
