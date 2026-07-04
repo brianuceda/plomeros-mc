d438eb838eb47d1c3c374224899daaa51737494d
Brian Uceda <brian.uceda@hotmail.com>
2026-07-04 18:31:10 -0500

    servidor v2

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 title Arclight 1.21.1 NeoForge Server
 echo Iniciando servidor...
 
-java -Xms4G -Xmx4G -jar arclight-neoforge-1.21.1-1.0.2-SNAPSHOT-0769551.jar nogui
+java -Xms6G -Xmx6G -jar arclight-neoforge-1.21.1-1.0.2-SNAPSHOT-0769551.jar nogui
 
 pause
